9c9ed44f90381045a1a55d6cb8049845289f2b27
ben-firestine <ben.firestine@gmail.com>
2020-12-02 18:33:31 -0500

    Add files via upload

---
+++ 2020/Day_1/AoC-Day1-P1.rb
@@ -0,0 +1,17 @@
+numbers = File.readlines('C:/Users/Ben Firestine/Google Drive/Wayne State/Advent of Code/2020/Day_1/input.txt')
+
+#loops through all the numbers to find a match
+numbers.each do |num1|
+  numbers.each do |num2|
+
+      #sums the numbers and prints them
+      sum = num1.to_i + num2.to_i
+      puts num1.strip + '+' + num2.strip + '=' + sum.to_s
+
+      #Checks to see if the numbers equal 2020
+      if sum == 2020
+        puts num1.to_i * num2.to_i
+        exit
+      end
+    end
+  end
