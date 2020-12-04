38f1bf071bf99244ea246992268d76397faf56a0
Ben Firestine <ben.firestine@gmail.com>
2020-12-04 16:35:01 -0500

    Adding part 2 in.

---
+++ 2020/Day_1/AoC-Day1-P2.rb
@@ -0,0 +1,19 @@
+numbers = File.readlines('C:\Users\Ben Firestine\github\Advent-of-Code\2020\Day_1\input.txt')
+
+#loops through all the numbers to find a match
+numbers.each do |num1|
+  numbers.each do |num2|
+    numbers.each do |num3|
+
+      #sums the numbers and prints them
+      sum = num1.to_i + num2.to_i + num3.to_i
+      puts num1.strip + '+' + num2.strip + '+' + num2.strip + '=' + sum.to_s
+
+      #Checks to see if the numbers equal 2020
+      if sum == 2020
+        puts num1.to_i * num2.to_i * num3.to_i
+        exit
+      end
+    end
+  end
+end
