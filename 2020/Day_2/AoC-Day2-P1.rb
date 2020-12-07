357a794268f8f415edb454fa2213acfc7deb0cd9
Ben Firestine <ben.firestine@gmail.com>
2020-12-07 15:41:48 -0500

    Finished Day1-P1 code

--- 2020/Day_2/AoC-Day2-P1.rb
+++ 2020/Day_2/AoC-Day2-P1.rb
@@ -1,18 +1,16 @@
 input = File.readlines('C:\Users\Ben Firestine\github\Advent-of-Code\2020\Day_2\input.txt')
 passwords = 0
 
-puts "#{input}"
-
 input. each do |item|
+  #Parse initial value
   policy, pass = item.split(':')
-
-  puts "#{policy}"
-  puts "#{pass}"
   policy_requirement, policy_letter = policy.split(' ')
 
+  #Get min and max requirement
   min = policy_requirement.split('-').first
   max = policy_requirement.split('-').last
 
+  #Checks password for issues
   if pass.count(policy_letter).between?(min.to_i, max.to_i)
     passwords += 1
   end
